86d71b817de7225b10a4f1c26959737cbea063d4
deepaksaroha <deepaksaroha1993@gmail.com>
2021-11-04 15:20:42 +0530

    updated encode.css for responsive behavior

--- encode.js
+++ encode.js
@@ -1,43 +1,115 @@
 window.addEventListener('load', () =>{
-    let subOptions = document.getElementById("sub-option");
+    let subop = document.getElementById("sub-option");    
+    const topop = document.getElementById("top-option");
 
     let topSelection = "1";
     let subSelection = "1";
     let toEncodeOrDecode = 1;
+
+
+    const topOpSelector = document.getElementById('encoder-selector');
+    topOpSelector.addEventListener('click', ()=>{
+        topop.style.visibility = 'visible';
+    })
+
+    const subOpSelector = document.getElementById('hash-selector');
+    subOpSelector.addEventListener('click', ()=>{
+        subop.style.display = 'block';
+        subop.style.visibility = 'visible';
+    })
+
+
+
+    const topOptions = document.querySelectorAll('.top-options');
+    topOptions.forEach(element => {
+        element.addEventListener('click', (e)=>{
+            console.log(e.target.innerText);
+            topOpSelector.innerText = e.target.innerText;
+            topop.style.visibility = 'hidden';
     
+            input.value = '';
+            output.value = '';
+            subSelection = "1";
+
+            
+            topSelection = e.target.id;
+
+            if(topSelection == "1" || topSelection == "2"){
+                subop.style.visibility = 'hidden';
+                subOpSelector.style.visibility = "hidden";
+                opearationSwitch.style.display = "inline-block";
+
+                toEncodeOrDecode = 1;
+                document.getElementById("operation-btn").innerText = "Encode";
+
+            }else{
+                subOpSelector.style.visibility = "visible";
+                subop.style.visibility = 'visible';
+                opearationSwitch.style.display = "none";
+
+                document.getElementById("operation-btn").innerText = "Hash";
+            }
+
+        })
+    })
+
+    const subOptions = document.querySelectorAll('.sub-options');
+    subOptions.forEach(element => {
+        element.addEventListener('click', (e)=>{
+            console.log(e.target.innerText);
+            subOpSelector.innerText = e.target.innerText;
+            subop.style.display = 'none';
+            subop.style.visibility = 'visible';
+            
+    
+            output.value = '';
+            input.value = '';
+
+            if(toEncodeOrDecode === 1){
+                toEncodeOrDecode = 0;
+                document.getElementById("operation-btn").innerText = "Decode";
+            }else{
+                toEncodeOrDecode = 1;
+                document.getElementById("operation-btn").innerText = "Encode";
+            }
+
+            
+            subop.style.display = 'none';
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     const input = document.getElementById("input-text");
     const output = document.getElementById("output-text");
 
-    const topop = document.getElementById("top-option");
-    topop.addEventListener("change", (e)=>{
-
-        console.log('sdsad');
+    topop.addEventListener("on", (e)=>{
         
-        input.value = '';
-        output.value = '';
-        subSelection = "1";
-
-        
-        topSelection = e.target.value;
-
-        if(topSelection == "1" || topSelection == "2"){
-            subOptions.style.display = "none";
-            opearationSwitch.style.display = "inline-block";
-            
-            toEncodeOrDecode = 1;
-            document.getElementById("operation-btn").innerText = "Encode";
-            
-        }else{
-            subOptions.style.display = "inline-block";
-            opearationSwitch.style.display = "none";
-
-            document.getElementById("operation-btn").innerText = "Hash";
-        }
 
     })
 
-    subOptions.addEventListener("change", (e)=>{
+    subop.addEventListener("change", (e)=>{
+        subop.style.visibility = 'hidden';
+        
         input.value = '';
         output.value = '';
 
@@ -47,16 +119,7 @@ window.addEventListener('load', () =>{
 
     const opearationSwitch = document.getElementById("operation-switch");
     opearationSwitch.addEventListener("click", ()=>{
-        output.value = '';
-        input.value = '';
-
-        if(toEncodeOrDecode === 1){
-            toEncodeOrDecode = 0;
-            document.getElementById("operation-btn").innerText = "Decode";
-        }else{
-            toEncodeOrDecode = 1;
-            document.getElementById("operation-btn").innerText = "Encode";
-        }
+        
     })
 
 
