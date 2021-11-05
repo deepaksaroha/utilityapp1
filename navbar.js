e1623fdcd70a31b21e967caf5b47cdef74e83d81
deepaksaroha <deepaksaroha1993@gmail.com>
2021-11-05 12:27:53 +0530

    added links for new pages in navbar

--- navbar.js
+++ navbar.js
@@ -1,4 +1,55 @@
+let menuList = [
+    {
+        subMenuList: [],
+        href: './timer',
+        itemName: 'Timer'
+    },
+    {
+        subMenuList: [],
+        href: './stopwatch',
+        itemName: 'Stopwatch'
+    },
+    {
+        subMenuList: [],
+        href: './ip',
+        itemName: 'My IP'
+    },
+    {
+        subMenuList: [],
+        href: './linguist',
+        itemName: 'Linguist'
+    }
+]
+
+
 window.addEventListener('load', () => {
+    
+    const menuItemBox = document.querySelector('#navbar-menu-box');
+    console.log(Object.keys(menuList));
+    menuList.forEach(element=>{
+        let menu = document.createElement('div');
+        menu.setAttribute('class', 'menu');
+        let menuItemName = document.createElement('a');
+        menuItemName.setAttribute('href', element.href);
+        menuItemName.innerText = element.itemName;
+        let btn = document.createElement('button');
+        let img = document.createElement('img');
+        img.setAttribute('src', './expand.svg');
+        btn.appendChild(img);
+        let subMenu = document.createElement('div');
+        subMenu.setAttribute('class', 'submenu');
+        element.subMenuList.forEach(ele=>{
+            let subMenuItemName = document.createElement('a');
+            subMenuItemName.setAttribute('href', element.href);
+            subMenuItemName.innerText = ele;
+            subMenu.appendChild(subMenuItemName);
+        })
+        menu.appendChild(menuItemName);
+        menu.appendChild(btn);
+        menu.appendChild(subMenu);
+        menuItemBox.appendChild(menu);
+    })
+
 
     const breadIcon = document.getElementById('menu-icon');
     let displayNavbar = false;
